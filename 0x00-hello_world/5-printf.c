aa2c3480165c4e97d3f4138bc627396b493e5440
niikels94 <niiamukekeli@gmail.com>
2022-04-07 17:34:58 -0700

    My 5-printf.c commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+/**
+ *main -execution and termination of program
+ *
+ *Return: ends main function
+ */
+int main(void) 
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
 
-int main() {
-	printf("with proper grammar, but the outcome is a piece of art\n");
 	return 0;
 }	
