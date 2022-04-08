7f17d839fbb258acc7882819ef19a83f914cd0d0
niikels94 <niiamukekeli@gmail.com>
2022-04-08 13:19:00 -0700

    My 2-print_alphabet.c commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - execution and termination of program
+ *
+ *Return: ends main function
+ */
+int main(void)
+{
+	cha c
+	for (c = 'A'; c <= 'Z'; ++c)
+	putchar("%c ", c);
+	return 0;
+}
