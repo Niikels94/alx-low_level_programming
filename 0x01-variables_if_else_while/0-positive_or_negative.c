883039154f3a7b5b9f2349958472c0390d2d62b8
niikels94 <niiamukekeli@gmail.com>
2022-04-08 14:38:55 -0700

    My 0-positive_or_negative.c commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,22 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 /**
  *main - execution and termination of program
  *
- *Return: ends main function
+ *Return: Nil return
  */
 int main(void)
 {
-	int n
-	printf("Enter a random number:");
-	scanf("%d", &n);
+	int n;
+	srand(time(0));
+	n =rand() -RAND_MAX/2;
 	if (n > 0) {
-		printf("is positive!\n");
+		printf("%d is positive!\n");
 	}
 	else if (n == 0) {
-		printf("is zero\n");
+		printf("%d is zero\n");
 	}
 	else {
-		print("is negative\n");
+		printf("%d is negative\n");
 	}
 	return (0);
 }
