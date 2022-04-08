313bd327668f97f1f669ca8c553d22c1e31cb241
niikels94 <niiamukekeli@gmail.com>
2022-04-07 17:00:42 -0700

    My 4-puts.c commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
- *  * main - execution and termination of program
+ *  *main - execution and termination of program
  *   *
- *    * Return: ends main function
+ *    *Return: ends main function
  *     */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
+----------------------------------------------------------------------------------------------------------
