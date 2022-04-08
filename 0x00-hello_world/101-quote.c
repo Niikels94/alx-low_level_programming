58b85aa57a830e4f723df47a8f8b38a5127c0000
niikels94 <niiamukekeli@gmail.com>
2022-04-08 05:45:15 -0700

    My 101-quote.c commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,12 @@
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n")
+	char str_1[] = "and that piece of art is useful";
+	char name[] = "Dora Korpar";
+	int year = 2015;
+	int month  10;
+	int day = 19;
+	
+	fprintf(stderr, "%s\" - %s, %d-%d-%d\n", str_1, name, year, month, day);
 	return (1);
 }
