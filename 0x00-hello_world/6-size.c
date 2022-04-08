a91327a895daea992ab7a0784c045683d6ae01d4
niikels94 <niiamukekeli@gmail.com>
2022-04-07 18:14:49 -0700

    My 6-size.c commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+nclude <stdio.h>
+/**
+ *main - execution and termination of program
+ *   
+ *Return: ends main function
+ */
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
