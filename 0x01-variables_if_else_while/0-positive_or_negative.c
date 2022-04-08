f73f1e1404907c5c29e77bdfd98e4026b2a8e0fe
niikels94 <niiamukekeli@gmail.com>
2022-04-08 12:01:42 -0700

    My 0-positive_or_negative.c commit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+	int n;
+	n = random numbers
+	printf("Enter a random number:");
+	scanf("%d", &n);
+	if (n > 0) {
+		printf("is positive!\n");
+	}
+	else if (n == 0) {
+		printf("is zero\n");
+	}
+	else {
+		print("is negative\n");
+	}
+	return (0);
+}
