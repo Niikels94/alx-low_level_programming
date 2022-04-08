ac89f17d513d4eee6307f30b7b9ef49d8b9f25ab
niikels94 <niiamukekeli@gmail.com>
2022-04-08 15:40:09 -0700

    My 3-print_alphabets.c commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *main - execution 
+ *
+ *Return: Nil result
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	for (letter = 'A'; letter <= 'Z'; letter++)
+	{
+		putchar(letter);
+	}
+	
+	putchar('\n');
+	
+	0return (0);
+}
