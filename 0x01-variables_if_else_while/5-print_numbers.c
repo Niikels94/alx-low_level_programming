5ca04ce0d16774bca05e75a79a0f967a70828278
niikels94 <niiamukekeli@gmail.com>
2022-04-08 18:07:40 -0700

    My 5-print_numbers.c commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - execution
+ *
+ *Return: Nil result
+ */
+int main(void)
+{
+	int figures;
+
+	for (figures = 0; figures < 10; figures++)
+	{
+		printf("%d", figures);
+	}
+	printf("\n");
+
+	return (0);
+}
