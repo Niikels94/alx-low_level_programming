6497f13a236dd32cb632dea0d2a034f0a1597b9c
niikels94 <niiamukekeli@gmail.com>
2022-04-08 18:54:42 -0700

    My 9-print_comb.c commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ *main - execution
+ *
+ *Return: Nil result
+ */	
+int main(void)
+{
+	int figures;
+
+	for (figures = 48; figures < 58; figures++)
+	{
+		putchar(figures);
+		if (figures == 57)
+		{
+			break;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar(' ');
+
+	return (0);
+}
