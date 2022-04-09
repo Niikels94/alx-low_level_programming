3a78fd8dbda37b4dc5ca6ac7dddbe0b5b06af785
niikels94 <niiamukekeli@gmail.com>
2022-04-08 18:19:08 -0700

    My 6-print_numberz.c commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - execution
+ *
+ *Return: Nil result
+ */
+int main(void)
+{
+	int figures;
+
+	for (figures = 48; figures < 58; figures++)
+	{
+		putchar(figures);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
