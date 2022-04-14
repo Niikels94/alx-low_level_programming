c1b3ad386497ccdb223ae9540daf6c29e5cb9935
niikels94 <niiamukekeli@gmail.com>
2022-04-14 05:31:35 -0700

    My 1-main.c commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,17 +1,25 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * main - execution of code
+ * main - causes an infinite loop
  *
- * Return: Nil result
+ * Return: 0
  */
 
 int main(void)
 {
 	int i;
 
+	printf("Infinite loop incoming :(\n");
+
 	i = 0;
-	positive_or_negative(i);
+
+	/*while (i < 10)*/
+	/*{*/
+	/*putchar(i);*/
+	/*}*/
+
+	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
 }
