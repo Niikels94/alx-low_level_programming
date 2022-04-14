c3837b182f500b6eb381e72824214599dceb7f2a
niikels94 <niiamukekeli@gmail.com>
2022-04-14 02:56:51 -0700

    My 0-main.c, main.h commit

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,11 +3,9 @@
 
 #include <stdio.h>
 
-void postitive_or_negative(int i):
-int largest_number(int a, int b, int c);
 void postitive_or_negative(int i);
+int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 
 #endif
-clear
