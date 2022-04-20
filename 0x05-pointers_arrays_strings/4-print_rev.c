205738eff037f7d80d3520bdafafe4354cc3dd3d
niikels94 <niiamukekeli@gmail.com>
2022-04-20 11:48:28 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string.
+ *
+ *@s: the string to print.
+ *
+ */
+
+void print_rev(char *s);
+{
+	char k;
+
+	for (k = 'z'; k =< 'a'; k++)
+	{
+		_putchar(k);
+	}
+	_putchar('\n');
+}
