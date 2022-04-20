2303258486aa0dc36b7a317bcbecaddfa1ea2123
niikels94 <niiamukekeli@gmail.com>
2022-04-20 12:28:33 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * rev_string - reversed string
+ *
+ *@s: the string to print.
+ *
+ */
+
+void rev_string(char *s);
+
+{
+	int k, b, c;
+	char a1, a2;
+
+	for (k = 0; s[k] != '\0'; k++)
+	{
+	}
+
+	b = k - 1;
+	c = 0;
+
+	while (b > c)
+	{
+		a1 = s[c];
+		a2 = s[b];
+		s[c] = a2;
+		s[b] = a1;
+		b--;
+		c++;
+	}
+}
+
