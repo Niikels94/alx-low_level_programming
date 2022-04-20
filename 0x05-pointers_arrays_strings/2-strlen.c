2de04b30b98e27cd37ef2ca5b2241299dc10afe0
niikels94 <niiamukekeli@gmail.com>
2022-04-20 11:13:14 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strlen - compute the length of a string.
+ *
+ * @s: length of a string
+ * Return: Nil
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	int k = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		k++;
+	}
+
+	return (0);
+}
