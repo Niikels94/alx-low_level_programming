3219d5db9dcae1bdd5198ae04148c91658080ac7
niikels94 <niiamukekeli@gmail.com>
2022-04-24 10:36:06 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *rot13 - encodes a string using rot13.
+ *@s: the string to be encoded to rot13.
+ *Return: Nil
+ */
+
+char *rot13(char *s)
+{
+	int a = 0, b = 0;
+	char string_rot13[] = "NnOoPpQqRrSsTtUuVvWwXxYyZzAaBbCcDdEeFfGgHhIiJjKkLlMm";
+	char string_alpha[] = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+
+	while (s[a] != '\0')
+	{
+		do {
+			if (s[a] == string_alpha[b])
+			{
+				s[a] = string_rot13[b];
+				break;
+			}
+			j++;
+		}
+		 while (string_alpha[b] != '\0')
+			 b = 0;
+		 a++;
+	}
+
+	return (s);
+}
