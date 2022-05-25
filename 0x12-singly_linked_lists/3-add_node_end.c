25c32353cc37f9bfcaa554726b13d231a1cb3674
niikels94 <niiamukekeli@gmail.com>
2022-05-24 18:05:50 -0700

    My 3-add_node_end.c

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,55 @@
+#include "lists.h"
+
+/**
+ * _strlen - lenght of string
+ * @s:char
+ * Return:int
+ */
+unsigned int _strlen(char *s)
+{
+	int k;
+
+		for (k = 0; s[k] != '\0'; k++)
+		{
+			continue;
+		}
+return (k);
+}
+/**
+ * add_node_end - add new node at the end of the list
+ * @head:pointer to ponter to first list
+ * @str : ponter to string
+ * Return:pointer to a node at the end of the list
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *first, *last;
+
+	if (str == NULL)
+		return (NULL);
+	new = malloc(sizeof(list_t));
+	if (first == NULL)
+		return (NULL);
+	first->str = strdup(str);
+	if (first->str == NULL)
+	{
+		free(first);
+		return (NULL);
+	}
+	first->len = _strlen(first->str);
+	first->next = NULL;
+	if (*head == NULL)
+	{
+		*head = first;
+	}
+	else
+	{
+	last = *head;
+		while (last->next)
+		{
+		last = last->next;
+		}
+	last->next = first;
+	}
+	return (first);
+}
