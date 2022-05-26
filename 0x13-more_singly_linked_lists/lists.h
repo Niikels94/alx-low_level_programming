b86d41cac16045869acd69d1c2e05ea4101b1be9
niikels94 <niiamukekeli@gmail.com>
2022-05-26 12:51:22 -0700

    My lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,25 +1,18 @@
-#ifndef _LIST_H_
-#define _LIST_H_
-#include <stdio.h>
-#include <stddef.h>
+#ifndef LISTS_H
+#define LISTS_H
 #include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
 
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
+ * struct listint_s - singly linked list
+ * @n: integer
  * @next: points to the next node
- *
- * Description: singly linked list node structure
- * for Holberton project
  */
-typedef struct list_s
+typedef struct listint_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
-} list_t;
+	int n;
+	struct listint_s *next;
+} listint_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
@@ -32,6 +25,6 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
-int _putchar(char c);
-
+listint_t *reverse_listint(listint_t **head);
+size_t print_listint_safe(const listint_t *head);
 #endif
