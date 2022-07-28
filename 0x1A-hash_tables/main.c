6428fa0ac43f30890a61f97a52982148b2790286
niikels94 <niiamukekeli@gmail.com>
2022-07-28 13:56:14 -0700

    My Test

---
+++ 0x1A-hash_tables/main.c
@@ -0,0 +1,156 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * main - check the code for
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+    hash_table_t *ht;
+
+    ht = hash_table_create(1024);
+    printf("%p\n", (void *)ht);
+    return (EXIT_SUCCESS);
+}
+
+int main(void)
+{
+    char *s;
+    unsigned long int hash_table_array_size;
+
+    hash_table_array_size = 1024;
+    s = "cisfun";
+    printf("%lu\n", hash_djb2((unsigned char *)s));
+    printf("%lu\n", key_index((unsigned char *)s, hash_table_array_size));
+    s = "Don't forget to tweet today";
+    printf("%lu\n", hash_djb2((unsigned char *)s));
+    printf("%lu\n", key_index((unsigned char *)s, hash_table_array_size));
+    s = "98";
+    printf("%lu\n", hash_djb2((unsigned char *)s));
+    printf("%lu\n", key_index((unsigned char *)s, hash_table_array_size));
+    return (EXIT_SUCCESS);
+}
+int main(void)
+{
+    hash_table_t *ht;
+
+    ht = hash_table_create(1024);
+    hash_table_set(ht, "betty", "cool");
+    return (EXIT_SUCCESS);
+}
+int main(void)
+{
+    hash_table_t *ht;
+    char *value;
+
+    ht = hash_table_create(1024);
+    hash_table_set(ht, "c", "fun");
+    hash_table_set(ht, "python", "awesome");
+    hash_table_set(ht, "Bob", "and Kris love asm");
+    hash_table_set(ht, "N", "queens");
+    hash_table_set(ht, "Asterix", "Obelix");
+    hash_table_set(ht, "Betty", "Cool");
+    hash_table_set(ht, "98", "Battery Street");
+    hash_table_set(ht, "c", "isfun");
+
+    value = hash_table_get(ht, "python");
+    printf("%s:%s\n", "python", value);
+    value = hash_table_get(ht, "Bob");
+    printf("%s:%s\n", "Bob", value);
+    value = hash_table_get(ht, "N");
+    printf("%s:%s\n", "N", value);
+    value = hash_table_get(ht, "Asterix");
+    printf("%s:%s\n", "Asterix", value);
+    value = hash_table_get(ht, "Betty");
+    printf("%s:%s\n", "Betty", value);
+    value = hash_table_get(ht, "98");
+    printf("%s:%s\n", "98", value);
+    value = hash_table_get(ht, "c");
+    printf("%s:%s\n", "c", value);
+    value = hash_table_get(ht, "javascript");
+    printf("%s:%s\n", "javascript", value);
+    return (EXIT_SUCCESS);
+}
+
+int main(void)
+{
+    hash_table_t *ht;
+
+    ht = hash_table_create(1024);
+    hash_table_print(ht);
+    hash_table_set(ht, "c", "fun");
+    hash_table_set(ht, "python", "awesome");
+    hash_table_set(ht, "Bob", "and Kris love asm");
+    hash_table_set(ht, "N", "queens");
+    hash_table_set(ht, "Asterix", "Obelix");
+    hash_table_set(ht, "Betty", "Cool");
+    hash_table_set(ht, "98", "Battery Street");
+    hash_table_print(ht);
+    return (EXIT_SUCCESS);
+}
+
+int main(void)
+{
+    hash_table_t *ht;
+    char *key;
+    char *value;
+
+    ht = hash_table_create(1024);
+    hash_table_set(ht, "c", "fun");
+    hash_table_set(ht, "python", "awesome");
+    hash_table_set(ht, "Bob", "and Kris love asm");
+    hash_table_set(ht, "N", "queens");
+    hash_table_set(ht, "Asterix", "Obelix");
+    hash_table_set(ht, "Betty", "Cool");
+    hash_table_set(ht, "98", "Battery Streetz");
+    key = strdup("Tim");
+    value = strdup("Britton");
+    hash_table_set(ht, key, value);
+    key[0] = '\0';
+    value[0] = '\0';
+    free(key);
+    free(value);
+    hash_table_set(ht, "98", "Battery Street");
+    hash_table_set(ht, "hetairas", "Bob");
+    hash_table_set(ht, "hetairas", "Bob Z");
+    hash_table_set(ht, "mentioner", "Bob");
+    hash_table_set(ht, "hetairas", "Bob Z Chu");
+    hash_table_print(ht);
+    hash_table_delete(ht);
+    return (EXIT_SUCCESS);
+}
+
+int main(void)
+{
+    shash_table_t *ht;
+
+    ht = shash_table_create(1024);
+    shash_table_set(ht, "y", "0");
+    shash_table_print(ht);
+    shash_table_set(ht, "j", "1");
+    shash_table_print(ht);
+    shash_table_set(ht, "c", "2");
+    shash_table_print(ht);
+    shash_table_set(ht, "b", "3");
+    shash_table_print(ht);
+    shash_table_set(ht, "z", "4");
+    shash_table_print(ht);
+    shash_table_set(ht, "n", "5");
+    shash_table_print(ht);
+    shash_table_set(ht, "a", "6");
+    shash_table_print(ht);
+    shash_table_set(ht, "m", "7");
+    shash_table_print(ht);
+    shash_table_print_rev(ht);
+	printf("key n  = %s \n", shash_table_get(ht, "n"));
+	printf("key b  = %s \n", shash_table_get(ht, "b"));
+	printf("key z  = %s \n", shash_table_get(ht, "z"));
+	printf("key j  = %s \n", shash_table_get(ht, "j"));
+	printf("key c  = %s \n", shash_table_get(ht, "c"));
+        shash_table_delete(ht);
+    return (EXIT_SUCCESS);
+}
